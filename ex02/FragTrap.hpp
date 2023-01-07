ac35aa4bc82c7b58e30f818bc0dbe3224f0bdbf2
“brixxt27” <brixxt27@gmail.com>
2023-01-07 20:56:57 +0900

    feat: FragTrap 완성#5
    FragTrap 은 ScavTrap 과 다른 점이 attact() 과 guardGate() 가 없는 것과
    highFivesGuys() 가 있는 것이 다르다. 그래서 그대로 복사 후 이름만 바꾸는
    식으로 진행했다.

---
+++ ex02/FragTrap.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef FRAGTRAP_HPP
+#define FRAGTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class FragTrap : public ClapTrap
+{
+public:
+	FragTrap(std::string name);
+	FragTrap(const FragTrap& other);
+	FragTrap&	operator=(const FragTrap& rhs);
+	virtual ~FragTrap();
+
+	void highFivesGuys(void);
+};
+
+#endif
---
+++ ex02/main.cpp
@@ -0,0 +1,16 @@
+#include "ScavTrap.hpp"
+#include "FragTrap.hpp"
+
+int	main(void)
+{
+	FragTrap	frag1("frag1");
+
+	frag1.attack("clap1");	
+	frag1.attack("scav1");	
+	frag1.highFivesGuys();
+	frag1.takeDamage(10);
+	frag1.beRepaired(1);
+	frag1.takeDamage(100);
+	frag1.attack("clap1");	
+	frag1.highFivesGuys();
+}
